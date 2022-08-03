3b88eded50a0cdd08f44125451b34bf739bf344e
Yaser Adel Mehraban <adel.mehrban@gmail.com>
2022-08-03 13:18:49 +1000

    Added module 4 demos

---
+++ Module 4/Feature Flag/ContosoCars/Program.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Src
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      CreateHostBuilder(args).Build().Run();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host
+        .CreateDefaultBuilder(args)
+        .ConfigureWebHostDefaults(webBuilder =>
+          webBuilder.ConfigureAppConfiguration((hostingContext, config) =>
+          {
+            var settings = config
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: true)
+            .AddCommandLine(args)
+            .Build();
+            config.AddAzureAppConfiguration(options =>
+            {
+              options.Connect(settings["ConnectionStrings:AppConfig"])
+                      .UseFeatureFlags();
+            });
+          })
+        .UseStartup<Startup>());
+  }
+}
